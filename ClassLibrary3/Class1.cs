b1f27d7a0e57bd1393023253727a081a584d4880
shadrix <postilga.dima@gmail.com>
2020-11-26 16:54:55 +0200

    Update Class1.cs

--- ClassLibrary3/Class1.cs
+++ ClassLibrary3/Class1.cs
@@ -11,17 +11,17 @@ namespace ClassLibrary3
 {
     public class Starter
     {
-        public static void Run()
+        public static void Run() // почему он статик ?
         {
             for (int i = 1; i <= 100; i++)
             {
                 try
                 {
-                    Random r = new Random();
+                    Random r = new Random(); // var
                     int x = r.Next(1, 6);
-                    if (x % 5 == 0)
+                    if (x % 5 == 0) // что за стремная конструкция из if где switch case?
                     {
-                        throw new Exception("I broke a toilet");
+                        throw new Exception("I broke a toilet"); // это что вообще за логика страная? в задание же написано было что 3 метод КИАДЕТ ошибку а это что тут делает и вообще зачем это
                     }
                     else if (x % 3 == 0)
                     {
@@ -35,19 +35,20 @@ namespace ClassLibrary3
                 }
                 catch (Exception ex)
                 {
-                    Actions.Make_Exception(ex);
+                    Actions.Make_Exception(ex); // этого тут быть не должно тут должен вызываться логер который ошибку запишет
 
                 }
                 finally
                 {
-
+                    // этот блок ничего не делает и он не обязательный удали его
                 }
 
             }
+    
+            // это вообще что то стремное и не понятно зачем
+            /*Console.WriteLine("Продолжить(Ent) или закрыть консоль(Esc)?");
 
-            Console.WriteLine("Продолжить(Ent) или закрыть консоль(Esc)?");
-
-            var key = Console.ReadKey();
+            var key = Console.ReadKey(); // зачем ?
 
             switch (key.Key)
             {
@@ -57,7 +58,8 @@ namespace ClassLibrary3
                         string[] AllLogs = Logger.ReadAllLogs();
                         for (int i = 0; i < AllLogs.Length; i++)
                         {
-                            Console.WriteLine(AllLogs[i]);
+                            Console.WriteLine(AllLogs[i]); текущий класс ВООБЩЕ не должен знать о CW о нем знает только ЛОГГЕР
+                            
                         }
                         break;
                     }
@@ -66,7 +68,7 @@ namespace ClassLibrary3
                     {
                         return;
                     }
-            }
+            }*/
 
         }
     }
