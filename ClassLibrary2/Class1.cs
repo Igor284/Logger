c5e4c4e8eaafe689a00063be9f8978c588618918
shadrix <postilga.dima@gmail.com>
2020-11-26 16:50:23 +0200

    Update Class1.cs

--- ClassLibrary2/Class1.cs
+++ ClassLibrary2/Class1.cs
@@ -9,12 +9,14 @@ namespace ClassLibrary2
 {
     public class Actions
     {
-        public static void Start_method()
+        public static void Start_method()// почему они статик? для какой цели ? Опять же упоротое название 
         {
+            // по месседжу передаче текста и синглтону я уже отписал в логгера
+            // где var ?
             Logger logger = Logger.getInstance("Info","Youre here");
 
-            string to_console = logger.Make_Massage();
-            Console.WriteLine(to_console);
+            string to_console = logger.Make_Massage(); // var
+            Console.WriteLine(to_console); // упоротое название переменной и CW тут не должно быть 
         }
 
         public static void Skipped_logic()
@@ -26,6 +28,7 @@ namespace ClassLibrary2
 
         public static void Make_Exception(Exception ex)
         {
+            //этот метод НЕ принимает Эксепшн он тут БРОСАЕТ эксепшн наверх
             Logger logger = Logger.getInstance("Exeption", ex.StackTrace);
             string to_console = logger.Make_Massage();
             Console.WriteLine(to_console);
