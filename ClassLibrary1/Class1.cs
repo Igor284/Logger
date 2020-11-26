6bbf04c4100f8e03c36c3adb54bed8981ca46eac
shadrix <postilga.dima@gmail.com>
2020-11-26 16:46:38 +0200

    Update Class1.cs

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -9,17 +9,20 @@ namespace ClassLibrary1
 {
     public class Logger
     {
-        private static string file_path = "Logger.TXT";
-        public string Type;
-        public string Message;
+        private static string file_path = "Logger.TXT"; // ты собираешься потом это менять или же название всегда будет неизменым 
+        //если не изменным то почему не const и почему это static. Что за упоротое название?) тут так не принято через _
+        public string Type; // публичное поле? к черту инкапсуляцию
+        public string Message; // опять же 
 
 
-        private static Logger instance;
+        private static Logger instance; 
 
         private Logger()
         { }
 
-        public Logger(string type)
+        // что это !?)) если ты сделал публичные конструкторы то где же тогда синглтон? 
+        //ты же везде можешь сделать var a = new Logger("sdf"); и таких кучу наклепать
+       /* public Logger(string type)
         {
             Type = type;
         }
@@ -28,9 +31,14 @@ namespace ClassLibrary1
         {
             Type = type;
             Message = message;
-        }
+        } */
 
         //тест комент
+        
+        //этот объект ты создашь ОДИН раз потом когда будешь вызывать эту строчку то 
+        // instance.Type = type;
+        //    instance.Message = message;
+        // уже не выполниться
         public static Logger getInstance(string type, string message)
         {
 
@@ -46,13 +54,19 @@ namespace ClassLibrary1
 
         public string Make_Massage()
         {
+            // Логер пишет в консоль а не в  файл 
+            
+            // при этом Дополнительно сохраняет весь текст для отдачи
+            // но писать в диск не скажу что это противоречит условиям но лучше это делать в памяти в тот же стринг билдер писать
             string massage = "{ " + Type + " }: { " + Message + " }";
             File.AppendAllText(file_path, massage + "\n");
             return massage;
         }
 
-        public static string[] ReadAllLogs()
+        public static string[] ReadAllLogs() // зачему это статик ? да и зачем метод когда проще сделать свойство на возврат 
         {
+            //почему не var ?
+            // не называй так переменные используй camelCase
             string[] all_logs = File.ReadAllLines(file_path);
             return all_logs;
         }
